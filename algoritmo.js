af3b5dc4215c273cd6ae9b51a019ca50b6e44d96
noegunner <noesosa81@gmail.com>
2023-08-29 00:08:05 -0300

    terminado

--- algoritmo.js
+++ algoritmo.js
@@ -1,4 +1,10 @@
- const inputNuevaTarea = document.querySelector("#nueva-tarea")
+ const inputNuevaTarea = document.querySelector("#nueva-tarea");
+ const inputTareaBuscada = document.querySelector("#numero-tarea-buscada");
+
+ const inputPosTareaActualizar = document.querySelector("#numero-tarea-actualizar"); 
+ const inputTxtTareaActualizar = document.querySelector("#txt-tarea-actualizar");
+ 
+ const inputPosTareaEliminar = document.querySelector("#numero-tarea-eliminar");
  const CLAVE_TAREAS = "listaTareas";
  
  let listaTareas = "lista vacia";
@@ -8,20 +14,28 @@
     listaTareas = JSON.parse(localStorage.getItem(CLAVE_TAREAS));
     if (listaTareas === null){
         listaTareas = [];
-        localStorage.setItem(CLAVE_TAREAS,JSON.stringify(listaTareas));
-       /*  localStorage.setItem(CLAVE_TAREAS, JSON.stringify(listaTareas)); */
-        
+        actualizarLocalStorage();
     }
 
+    generarListaTareas();
+}
 
 
 
- }
+function actualizarLocalStorage() {
+    localStorage.setItem(CLAVE_TAREAS, JSON.stringify(listaTareas));
+}
 
 
 
-
-
+function generarListaTareas() {
+    document.querySelector("ol").innerHTML = "";
+    listaTareas.forEach(tarea => {
+        document.querySelector("ol").innerHTML += `
+         <li> ${tarea} </li>
+    `;
+    });
+}
 
 function agregar(){
    
@@ -36,5 +50,48 @@ function agregar(){
      localStorage.setItem(CLAVE_TAREAS,JSON.stringify(listaTareas));
     
 }
+
+function buscar(){
+   document.querySelector("#tarea-buscada").innerHTML = `
+    <span>Tarea buscada:</span>
+    <h4>${listaTareas[inputTareaBuscada.value - 1]}</h4>
+    `; 
+
+  }
+
+  function actualizar(){
+    let posTarea = inputPosTareaActualizar.value;
+    let txtNuevaTarea = inputTxtTareaActualizar.value;
+    
+    listaTareas[posTarea - 1] = txtNuevaTarea;
+
+    generarListaTareas();
+    actualizarLocalStorage();
+
+  }
+
+  function eliminar(){
+    let posTareaEliminar = inputPosTareaEliminar.value - 1;
+
+    listaTareas.splice(posTareaEliminar,1);
+
+    generarListaTareas();
+    actualizarLocalStorage();
+
+
+  }
+
+
+  function eliminarTodo(){
+   /*  localStorage.clear();
+    iniciarListaTareas(); */
+    listaTareas = [];
+    generarListaTareas();
+    actualizarLocalStorage();
+
+  }
+
+
+
 
 iniciarListaTareas();
